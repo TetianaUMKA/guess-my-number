9ddcccc3f7d3e243c932cf699226faf8fab4f923
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-31 22:45:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 10;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -41,6 +42,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🎉 Correct Number!';
       score--;
       document.querySelector('.score').textContent = score;
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
       document.querySelector('.number').textContent = secretNumber;
@@ -71,6 +76,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '💔 Game over';
     score--;
     document.querySelector('.score').textContent = score;
+    document.querySelector('.check').style.visibility = 'hidden';
   }
 });
 
